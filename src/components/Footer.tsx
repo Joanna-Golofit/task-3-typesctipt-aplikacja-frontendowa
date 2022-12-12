560a11dc2ada50c2b2b506f354b42190cb45c738
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-12 14:11:23 +0100

    typescript and modules - all works

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-// import classes from "./Footer.module.css";
-import "./Footer.css";
+import classes from "./Footer.module.css";
+// import "./Footer.css";
 
 
 const Footer = () => {
   return (
-    <footer className='.footer'>2022 All rights reserved</footer>
+    <footer className={classes.footer}>2022 All rights reserved</footer>
+    // <footer className='.footer'>2022 All rights reserved</footer>
   )
 }
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-// import classes from './Navigation.module.css'
-import './Navigation.css'
+import classes from './Navigation.module.css'
+// import './Navigation.css'
 
 
 const Navigation = () => {
   return (
-    <header className='.header'>
-      <nav>
-        <ul>
-          <li>
-            {/* <NavLink className={(navData) => navData.isActive && '.active'} to='/home'>Home</NavLink> */}
-            <NavLink to='/home'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='/aboutUs'>About us</NavLink>
-          </li>
-          <li>
-            <NavLink to='/contact'>Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+		<header className={classes.header}>
+			<nav>
+				<ul className={classes.ul}>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? classes.active : "")}
+							to="/home"
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? classes.active : "")}
+							to="/aboutUs"
+						>
+							About us
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className={navData => (navData.isActive ? classes.active : "")}
+							to="/contact"
+						>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Navigation
