56a13f121e65f3045ddacf9922908b4bccd8a928
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-17 15:06:05 +0100

    działa opóźnione...

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Locations } from '../models';
- import { ToastContainer, toast } from "react-toastify";
- import "react-toastify/dist/ReactToastify.css";
-import classes from './AboutUs.module.css';
+import React, { useState } from "react";
+import { Locations } from "../models";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import classes from "./AboutUs.module.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 interface Props {
 	locations: Locations[];
 	// jakasFunkcja: () => void;
@@ -12,22 +13,21 @@ interface Props {
 // e: React.FormEvent
 
 const AboutUs: React.FC<Props> = ({ locations }: Props) => {
+	const [copied, setCopied] = useState(false);
+	const [copiedValue, setCopiedValue] = useState("proba");
 	// const notify = (event: React.ChangeEvent<HTMLLIElement>) => {
 	const notify = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-		// const notify = (event: React.MouseEventHandler<HTMLInputElement>) => {
-		// const notify = (e: HTMLInputElement) => {
-		// const notify = (e: { target: HTMLInputElement }) => {
-		// const li = event.target;
-		// console.log(li);
 		const value = (event.target as HTMLLIElement).innerHTML;
-		// console.log((event.target as HTMLLIElement).innerHTML);
 		// toast.success("Copy to clipboard succeeded", {
 		// 	theme: "dark",
 		// });
+		console.log("clg", value);
+		setCopiedValue(value);
 		toast.success(value, {
 			theme: "dark",
 		});
 	};
+
 	// const notify2 = () => toast.error("Copy to clipboard failed!");
 
 	// toast.error("Lorem ipsum dolor");
@@ -35,14 +35,14 @@ const AboutUs: React.FC<Props> = ({ locations }: Props) => {
 
 	return (
 		<div>
-			{/* <button onClick={notify}>Notify!</button> */}
 			<ToastContainer autoClose={3000} closeOnClick position="bottom-right" />
 			<h1>AboutUs</h1>
 			<b>
 				Każda lokalizacja powinna posiadać możliwość skopiowania jej do schowka
 				przy pomocy buttona/icony kopiowania
 			</b>
-			{/* <input onClick={notify}></input> */}
+
+			{/* <span>Copy to clipboard </span> */}
 			<ul>
 				{locations.map(location => (
 					<li
@@ -51,13 +51,23 @@ const AboutUs: React.FC<Props> = ({ locations }: Props) => {
 						key={location.zip}
 						onClick={notify}
 					>
-						{location.country} {location.street} {location.zip} {location.city}
-						{/* {`${location.country} ${location.street} ${location.zip} ${location.city}`} */}
+						<CopyToClipboard
+							text={copiedValue}
+							// onCopy={event => {
+							// 	setCopiedValue(event);
+							// 	console.log("CopyToClipboard", event);
+							// }}
+						>
+							{/* {location.country} {location.street} {location.zip}	{location.city} */}
+							<p>
+								{`${location.country} ${location.street} ${location.zip} ${location.city}`}
+							</p>
+						</CopyToClipboard>
 					</li>
 				))}
 			</ul>
 		</div>
 	);
-}
+};
 
-export default AboutUs
+export default AboutUs;
