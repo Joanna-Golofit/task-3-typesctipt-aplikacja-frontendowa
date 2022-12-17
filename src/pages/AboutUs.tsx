8fec116e26070b36820d07bfbda6ff3181462e0f
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-17 13:49:14 +0100

    event.target.value caught!

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Locations } from '../models';
-
+ import { ToastContainer, toast } from "react-toastify";
+ import "react-toastify/dist/ReactToastify.css";
+import classes from './AboutUs.module.css';
 interface Props {
 	locations: Locations[];
 	// jakasFunkcja: () => void;
@@ -9,18 +11,48 @@ interface Props {
 
 // e: React.FormEvent
 
-const AboutUs: React.FC<Props> = ({locations}: Props) => {
-  return (
+const AboutUs: React.FC<Props> = ({ locations }: Props) => {
+	// const notify = (event: React.ChangeEvent<HTMLLIElement>) => {
+	const notify = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+		// const notify = (event: React.MouseEventHandler<HTMLInputElement>) => {
+		// const notify = (e: HTMLInputElement) => {
+		// const notify = (e: { target: HTMLInputElement }) => {
+		// const li = event.target;
+		// console.log(li);
+		const value = (event.target as HTMLLIElement).innerHTML;
+		// console.log((event.target as HTMLLIElement).innerHTML);
+		// toast.success("Copy to clipboard succeeded", {
+		// 	theme: "dark",
+		// });
+		toast.success(value, {
+			theme: "dark",
+		});
+	};
+	// const notify2 = () => toast.error("Copy to clipboard failed!");
+
+	// toast.error("Lorem ipsum dolor");
+	// toast.success("Lorem ipsum dolor");
+
+	return (
 		<div>
+			{/* <button onClick={notify}>Notify!</button> */}
+			<ToastContainer autoClose={3000} closeOnClick position="bottom-right" />
 			<h1>AboutUs</h1>
 			<b>
-				Każda lokalizacja powinna posiadać możliwość skopiowania jej do schowka przy
-				pomocy buttona/icony kopiowania
+				Każda lokalizacja powinna posiadać możliwość skopiowania jej do schowka
+				przy pomocy buttona/icony kopiowania
 			</b>
+			{/* <input onClick={notify}></input> */}
 			<ul>
 				{locations.map(location => (
-					<li key={location.zip}>
+					<li
+						className={classes.location}
+						title="Click to copy to the clipboard"
+						key={location.zip}
+						onClick={notify}
+					>
 						{location.country} {location.street} {location.zip} {location.city}
+						{/* {`${location.country} ${location.street} ${location.zip} ${location.city}`} */}
 					</li>
 				))}
 			</ul>
